4346ec20a5be25caaebe8cd0600255c553f406ba
PaulyD97 <78657855+PaulyD97@users.noreply.github.com>
2024-09-02 12:28:57 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -15,7 +15,7 @@ st.write("Diese App bietet die Möglichkeit Wortassoziationen in den Bundestagsr
          "ntersuchen. Die Nutzer:innen haben die Möglichkeit ein Wort auszuwählen. Dann wird ein Netzwerk angezeigt, da"
          "s aus 40 Wörtern besteht und die Wörter mit der höchsten Assoziation zu dem ausgewählten Wort beinhaltet."
         "Die Assoziation eines Worts mit einem anderen Wort wird von der Häufigkeit abgeleitet, wie oft die zwei Wörter mit einem"
-        "Abstand von maximal 20 Worten zusammen in den Bundestagsreden vorkommen.")
+        " Abstand von maximal 20 Worten zusammen in den Bundestagsreden vorkommen.")
 
 st.write("Es werden nicht zwischen allen 40 Wörtern bzw. Knoten die zugehörigen Assoziationen als Kanten abgebildet, sondern lediglic"
          "h die stärksten 10 % der Assoziationen. Die Dicke der Kanten ist die logarithmierte Stärke der Assoziation. "
