eecbf918c398946486ec7bd2852cdc325b418767
PaulyD97 <78657855+PaulyD97@users.noreply.github.com>
2024-09-03 13:23:01 +0200

    Update 3_Readme.py

--- pages/3_Readme.py
+++ pages/3_Readme.py
@@ -4,6 +4,7 @@ from pathlib import Path
 absolute_path = Path("dataReadme/network_rechtsextrem.PNG").resolve()
 
 markdown_content = Path("README.md").read_text()
+markdown_content = markdown_content.replace("dataReadme/network_rechtsextrem.PNG", "network_rechtsextrem.PNG")
 
 st.markdown(markdown_content)
 
