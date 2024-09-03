09d19c914e5c73b49d858984f023ba96cbbbdc8e
PaulyD97 <78657855+PaulyD97@users.noreply.github.com>
2024-09-03 13:10:14 +0200

    Update 3_Readme.py

--- pages/3_Readme.py
+++ pages/3_Readme.py
@@ -2,4 +2,6 @@ import streamlit as st
 from pathlib import Path
 
 markdown_content = Path("README.md").read_text()
-markdown_content = markdown_content.replace("/dataReadme/network_rechtsextrem.PNG", "./dataReadme/network_rechtsextrem.PNG")
+markdown_content = markdown_content.replace("/dataReadme/network_rechtsextrem.PNG", "../dataReadme/network_rechtsextrem.PNG")
+
+st.markdown(markdown_content)
