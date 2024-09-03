f68b5dcccefc9d664e5d1eb35ec1e23985f719bb
PaulyD97 <78657855+PaulyD97@users.noreply.github.com>
2024-09-03 13:18:09 +0200

    Update 3_Readme.py

--- pages/3_Readme.py
+++ pages/3_Readme.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from pathlib import Path
 
+absolute_path = Path("dataReadme/network_rechtsextrem.PNG").resolve()
+
 markdown_content = Path("README.md").read_text()
-markdown_content = markdown_content.replace("dataReadme/network_rechtsextrem.PNG", "network_rechtsextrem.PNG")
+markdown_content = markdown_content.replace("dataReadme/network_rechtsextrem.PNG", str(absolute_path))
 
 st.markdown(markdown_content)
 
-absolute_path = Path("dataReadme/network_rechtsextrem.PNG").resolve()
 st.image(str(absolute_path))
