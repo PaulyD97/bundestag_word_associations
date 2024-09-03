613c905f296e917b62bd97b2cb3ba7b13f3607a9
PaulyD97 <78657855+PaulyD97@users.noreply.github.com>
2024-09-03 13:11:57 +0200

    Update 3_Readme.py

--- pages/3_Readme.py
+++ pages/3_Readme.py
@@ -4,4 +4,4 @@ from pathlib import Path
 markdown_content = Path("README.md").read_text()
 st.markdown(markdown_content)
 
-st.image("../dataReadme/network_rechtsextrem.PNG")
+st.image("/dataReadme/network_rechtsextrem.PNG")
